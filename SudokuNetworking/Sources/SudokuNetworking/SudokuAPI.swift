84a52af10aa0c6cb83befa0ce78744ab55dafa43
Ilia Shapovalov <greateman@mail.ru>
2024-06-09 22:51:48 +0300

    [ADD] Logging

--- SudokuNetworking/Sources/SudokuNetworking/SudokuAPI.swift
+++ SudokuNetworking/Sources/SudokuNetworking/SudokuAPI.swift
@@ -1,6 +1,16 @@
 import Foundation
 import SwiftFP
 
+/// Класс `SudokuAPI` предоставляет механизм взаимодействия с несколькими ресурсами-поставщиками судоку.
+///
+/// `SudokuAPI` можно использовать в формате синглтона, через статическое свойство `SudokuAPI.shared`.
+/// Так же, для более тонкой настройки, можно инициализировать экземпляр `SudokuAPI` с кастомным
+/// конфигом (`URLSessionConfiguration`) и/или механизмом логирования.
+///
+/// Встроенный логер `SudokuAPI` возвращает словать с ключами: `"Object"`, `"Event"`  и `"Error"`.
+///  - Object: содержит имя объекта `SudokuAPI`
+///  - Event: текущий логируемый ивент
+///  - Error: состояние объекта и/или текущего обрабатываемого ивента
 public final class SudokuAPI {
     public static let shared = SudokuAPI()
     
@@ -47,6 +57,7 @@ private extension SudokuAPI {
             .composeUrl()
             .flatMap(Request.new(_:))
             .setMethod(method)
+            .map(log(request:))
     }
     
     func perform<T: Decodable>(request: URLRequest) async -> Result<T, SudokuError> {
@@ -62,17 +73,42 @@ private extension SudokuAPI {
         guard let httpResponse = response.response as? HTTPURLResponse else {
             preconditionFailure()
         }
+        let event = "Receive response:\n".appending(String(reflecting: httpResponse))
         if let error = SudokuError(httpResponse.statusCode) {
+            log(event: event, error: error)
             throw error
         }
+        log(event: event)
         return response.data
     }
     
-    func log(event: String) {
+    func log<Model>(
+        processing request: URLRequest
+    ) -> (Result<Model, SudokuError>) -> Result<Model, SudokuError> {
+        { [self] result in
+            let event = "Process response: "
+            switch result {
+            case .success:
+                log(event: event.appending("success"))
+                
+            case let .failure(error):
+                log(event: event.appending("failure"), error: error)
+            }
+            return result
+        }
+    }
+    
+    func log(request: URLRequest) -> URLRequest {
+        log(event: "Send Request:\n".appending(String(reflecting: request)))
+        return request
+    }
+    
+    func log(event: String, error: Error? = nil) {
         logger?(
             [
                 "Object" : String(describing: self),
-                "Event" : event
+                "Event" : event,
+                "Error" : String(reflecting: error)
             ]
         )
     }
